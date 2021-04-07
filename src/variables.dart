c99c477ee97cb962952bb5e5abf7f4238ceac7a0
naolArega <na408498@gmail.com>
2021-04-07 21:45:21 +0300

    variables and constants

---
+++ src/variables.dart
@@ -0,0 +1,15 @@
+// dynamic typed variables
+var foo = 'hello world!';
+dynamic bar = 'foo bar';
+
+// static typed variables
+int number = 24;
+String string = 'xyzzy';
+
+// constants
+
+// main entry point
+main() {
+  print('foo: ${foo}');
+  print('bar: ${bar}');
+}
