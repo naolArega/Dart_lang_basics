b493e9bbdd68297328582dedb6d791987bd81c99
naolArega <na408498@gmail.com>
2021-04-07 21:32:49 +0300

    primitive datatypes in dart

---
+++ src/datatypes.dart
@@ -0,0 +1,20 @@
+// number data type
+int integer = 10;
+double doublePrecision = 3.15;
+
+// string data type
+String stringType = 'hello';
+Runes runesType = new Runes('world');
+
+// boolean data type
+bool booleanType = true;
+
+main() {
+  print('integer: ${integer}');
+  print('double: ${doublePrecision}');
+
+  print('string: ${stringType}');
+  print('runes: ${runesType}');
+
+  print('boolean: ${booleanType}');
+}
