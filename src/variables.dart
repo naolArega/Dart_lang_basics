fd45f428c157bea04238c8d2964de926bebe504e
naolArega <na408498@gmail.com>
2021-04-08 20:15:17 +0300

    variables and constants

--- src/variables.dart
+++ src/variables.dart
@@ -7,9 +7,17 @@ int number = 24;
 String string = 'xyzzy';
 
 // constants
+const double pi = 3.15;
+final String name = 'naol';
 
 // main entry point
 main() {
   print('foo: ${foo}');
   print('bar: ${bar}');
+
+  print('static number: ${number}');
+  print('static string: ${string}');
+
+  print('constant pi: ${pi}');
+  print('contant name: ${name}');
 }
