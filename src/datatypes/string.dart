6063f1acc3f55c202148ae56dd8eb7045d09098e
naolArega <na408498@gmail.com>
2021-05-20 19:43:00 +0300

    strings

---
+++ src/datatypes/string.dart
@@ -0,0 +1,6 @@
+main() {
+  String foo = "hello world";
+  print("foo's codeunit ${foo.codeUnits}");
+  print("is foo empty, ${foo.isEmpty ? 'yes' : 'no'}");
+  print("foo is ${foo.length} characters long");
+}
