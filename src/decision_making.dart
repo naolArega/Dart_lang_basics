019312f01aa81664c3e01ae650934dfd08879f94
naolArega <na408498@gmail.com>
2021-05-20 19:22:23 +0300

    decision making

---
+++ src/decision_making.dart
@@ -0,0 +1,31 @@
+main() {
+  if (90 < 80) {
+    print('90 is less than 80');
+  } else {
+    print('but it is not');
+    if (90 > 80) {
+      print('it is actually greater than 80');
+    }
+  }
+
+  var foo = 90;
+
+  switch (foo) {
+    case 10:
+      {
+        break;
+      }
+    case 40:
+      {
+        break;
+      }
+    case 90:
+      {
+        break;
+      }
+    default:
+      {
+        break;
+      }
+  }
+}
