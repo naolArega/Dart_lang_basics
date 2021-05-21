0f290b3722fc82679e5ac02dd87e6037932bdddd
naolArega <na408498@gmail.com>
2021-05-21 14:20:33 +0300

    more of list

--- src/datatypes/list.dart
+++ src/datatypes/list.dart
@@ -1,8 +1,17 @@
 main(List<String> args) {
-  var foo = [];
-  foo[4] = 45;
+  // Lists and thier properties
+  var foo = [12, 41, 54, 77, 83.2, 'hello'];
+
+  print('the length of the List is ${foo.length}');
+  print('the first element is ${foo.first}');
+  print('foo is${foo.isNotEmpty ? ' Not' : ' '} empty');
 
   for (var bar in foo) {
     print(bar);
   }
+
+  // List methods
+  List bar = [23, 'world', 42.91];
+
+  bar.add('foo bar');
 }
