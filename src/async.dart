6ddaea08baa8a812db2b5fd3fae430847016a6f5
naolArega <na408498@gmail.com>
2021-05-23 19:35:56 +0300

    async wait future

--- src/async.dart
+++ src/async.dart
@@ -1,11 +1,25 @@
 import 'dart:async';
 import 'dart:io';
 
+dataRetriever(File file) async {
+  print('or ${await file.readAsString()}');
+}
+
 main(List<String> args) {
   File file = new File('${Directory.current.path}\\src\\assets\\docs.txt');
   Future<String> f = file.readAsString();
 
   f.then((value) => print(value));
+
+  // alternative
+
+  () async {
+    print('alternatively ${await file.readAsString()}');
+  }();
+
+  // or
+
+  dataRetriever(file);
 
   print('program terminated...');
 }
